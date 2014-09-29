672a618fd254f2409227c9f2f84c738e19270bd9
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-29 01:44:28 -0600

    added load_order method and adjusted order_item create action

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ActiveRecord::Base
+  validates :status, presence: true
+
+  has_many :order_items
+  has_many :items, through: :order_items
+  belongs_to :user
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,6 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
 
   validates :order_id, :product_id, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
 end
