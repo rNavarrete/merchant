ed6ff865b5dc4e355b591b7ae317f1d21a8cc63a
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-28 22:05:12 -0600

    added migration to add stock column to product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929022638) do
+ActiveRecord::Schema.define(version: 20140929033835) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140929022638) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stock",                               default: 0
   end
 
 end
