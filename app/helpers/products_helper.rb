42d4bd77a72bffe1f14c2220a70820edb181aa80
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-28 22:51:26 -0600

    fixed print_stock method and added stock field validations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,7 @@
 module ProductsHelper
   def print_stock(stock)
-
     if stock > 0
-      content_tag(:span, "In Stock(<%= product.stock %>)", class: "in_stock")
+      content_tag(:span, "In Stock(#{stock})", class: "in_stock")
     else
       content_tag(:span, "Out of stock", class: "out_of_stock")
     end
